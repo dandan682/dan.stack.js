62d99bab8688616c24ca9199a66dfcf954cd8557
dan <dandan682@outlook.com>
2021-04-02 17:19:41 -0600

    create switch.js

---
+++ src/switch.js
@@ -0,0 +1,15 @@
+const dayOfTheWeek = prompt("What day is today?")
+
+switch (dayOfTheWeek) {
+    case "monday":
+        console.log("I'm going to rest")
+        break
+    case "wednesday":
+        console.log("Lesson with Piero")
+        break
+    case "saturday":
+        console.log("I'ts movie night")
+        break
+    default:
+        console.log("No idea what to do")
+    }
