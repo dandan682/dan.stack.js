02765c0977052dc2f57f5a89662577355c44f35a
dan <dandan682@outlook.com>
2021-04-04 13:07:07 -0500

    update tellFortune

---
+++ src/tellFortune.js
@@ -0,0 +1,8 @@
+function tellFortune(num_children, partner_name, location, job) {
+    console.log("You will be a", job, "in", location, "and married to", 
+    partner_name, "with", num_children, "kids.")
+}
+
+tellFortune(3, "Sunil", "London", "teacher");
+tellFortune(2, "Piero", "Italy", "singer");
+tellFortune(0, "Dan", "Mexico", "engineer");
