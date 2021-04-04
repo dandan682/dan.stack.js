1ce78c06dba8de90aae014981748b56fa0cd0e54
dan <dandan682@outlook.com>
2021-04-04 16:59:12 -0500

    update geom.js

---
+++ src/geom.js
@@ -0,0 +1,12 @@
+function calcCircumfrence(radius) {
+    let Circumfrence = radius * 3.1416 * 2;
+    console.log("The circumference is", Circumfrence)
+}
+function calcArea(radius) {
+    let area = radius**2 * 3.1416
+    console.log("The area is", area)
+}
+
+let radius = 3;
+calcCircumfrence(radius);
+calcArea(radius);
