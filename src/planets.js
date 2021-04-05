9316604352df80418966ad1ccf87b961965d4df1
dan <dandan682@outlook.com>
2021-04-04 22:51:25 -0500

    update planets.js

---
+++ src/planets.js
@@ -0,0 +1,7 @@
+function listPlanets() {
+    let planets = ["Mercurio", "Venus", "Tierra", "Marte", "Jupiter"];
+    for (let i = 0; i < planets.length; i++) {
+        console.log(i,planets[i]);
+    }
+}
+listPlanets();
