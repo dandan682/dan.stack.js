1713f6b6e320b1cb597db843d96850ef68316e7f
dan <dandan682@outlook.com>
2021-04-04 22:51:06 -0500

    update alphabet.js

---
+++ src/alphabet.js
@@ -0,0 +1,10 @@
+function myAlphabetLength() {
+    let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+//    let myAlphabet = ['A', 'B', 'C'];
+    console.log("Length of the array is", myAlphabet.length);
+    if (myAlphabet.length < 4) { 
+        console.log("Array length is less than 4.")
+    }
+}
+
+myAlphabetLength();
